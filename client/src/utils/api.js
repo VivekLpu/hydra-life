fac038d0e861847e70ce205a83efa79379a73610
Vivek Kumar <118115653+VivekLpu@users.noreply.github.com>
2025-08-23 17:29:17 +0530

    Update api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -3,7 +3,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://hydra-life-server.onrender.com",
   withCredentials: false,
 });
 
